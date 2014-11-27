6f021c6d013fa1dc45566a2e85fc0180692b3a6d
kujenga <ataylor0123@gmail.com>
2014-11-27 11:10:30 +0100

    working starter code

--- incremental/main.cpp
+++ incremental/main.cpp
@@ -6,10 +6,303 @@
 //  Copyright (c) 2014 Aaron Taylor. All rights reserved.
 //
 
-#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+// Needed on MsWindows
+#include <windows.h>
+#endif // Win32 platform
+
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+// Download glut from: http://www.opengl.org/resources/libraries/glut/
+#include <GLUT/glut.h>
+
+#include "float2.h"
+#include "float3.h"
+#include <vector>
+#include <map>
+
+class LightSource
+{
+public:
+    virtual float3 getpowerDensityAt  ( float3 x )=0;
+    virtual float3 getLightDirAt  ( float3 x )=0;
+    virtual float  getDistanceFrom( float3 x )=0;
+    virtual void   apply( GLenum openglLightName )=0;
+};
+
+class DirectionalLight : public LightSource
+{
+    float3 dir;
+    float3 powerDensity;
+public:
+    DirectionalLight(float3 dir, float3 powerDensity)
+    :dir(dir), powerDensity(powerDensity){}
+    float3 getpowerDensityAt  ( float3 x ){return powerDensity;}
+    float3 getLightDirAt  ( float3 x ){return dir;}
+    float  getDistanceFrom( float3 x ){return 900000000;}
+    void   apply( GLenum openglLightName )
+    {
+        float aglPos[] = {dir.x, dir.y, dir.z, 0.0f};
+        glLightfv(openglLightName, GL_POSITION, aglPos);
+        float aglZero[] = {0.0f, 0.0f, 0.0f, 0.0f};
+        glLightfv(openglLightName, GL_AMBIENT, aglZero);
+        float aglIntensity[] = {powerDensity.x, powerDensity.y, powerDensity.z, 1.0f};
+        glLightfv(openglLightName, GL_DIFFUSE, aglIntensity);
+        glLightfv(openglLightName, GL_SPECULAR, aglIntensity);
+        glLightf(openglLightName, GL_CONSTANT_ATTENUATION, 1.0f);
+        glLightf(openglLightName, GL_LINEAR_ATTENUATION, 0.0f);
+        glLightf(openglLightName, GL_QUADRATIC_ATTENUATION, 0.0f);
+    }
+};
+
+class PointLight : public LightSource
+{
+    float3 pos;
+    float3 power;
+public:
+    PointLight(float3 pos, float3 power)
+    :pos(pos), power(power){}
+    float3 getpowerDensityAt  ( float3 x ){return power*(1/(x-pos).norm2()*4*3.14);}
+    float3 getLightDirAt  ( float3 x ){return (pos-x).normalize();}
+    float  getDistanceFrom( float3 x ){return (pos-x).norm();}
+    void   apply( GLenum openglLightName )
+    {
+        float aglPos[] = {pos.x, pos.y, pos.z, 1.0f};
+        glLightfv(openglLightName, GL_POSITION, aglPos);
+        float aglZero[] = {0.0f, 0.0f, 0.0f, 0.0f};
+        glLightfv(openglLightName, GL_AMBIENT, aglZero);
+        float aglIntensity[] = {power.x, power.y, power.z, 1.0f};
+        glLightfv(openglLightName, GL_DIFFUSE, aglIntensity);
+        glLightfv(openglLightName, GL_SPECULAR, aglIntensity);
+        glLightf(openglLightName, GL_CONSTANT_ATTENUATION, 0.0f);
+        glLightf(openglLightName, GL_LINEAR_ATTENUATION, 0.0f);
+        glLightf(openglLightName, GL_QUADRATIC_ATTENUATION, 0.25f / 3.14f);
+    }
+};
+
+class Material
+{
+public:
+    float3 kd;			// diffuse reflection coefficient
+    float3 ks;			// specular reflection coefficient
+    float shininess;	// specular exponent
+    Material()
+    {
+        kd = float3(0.5, 0.5, 0.5) + kd * 0.5;
+        ks = float3(1, 1, 1);
+        shininess = 15;
+    }
+    virtual void apply()
+    {
+        float aglDiffuse[] = {kd.x, kd.y, kd.z, 1.0f};
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, aglDiffuse);
+        float aglSpecular[] = {kd.x, kd.y, kd.z, 1.0f};
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, aglSpecular);
+        if(shininess <= 128)
+            glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+        else
+            glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 128.0f);
+    }
+};
+
+// Skeletal Camera class. Feel free to add custom initialization, set aspect ratio to fit viewport dimensions, or animation.
+class Camera
+{
+    float3 eye;
+    
+    float3 ahead;
+    float3 right;
+    float3 up;
+    
+    float fov;
+    float aspect;
+public:
+    float3 getEye()
+    {
+        return eye;
+    }
+    Camera()
+    {
+        eye = float3(0, 0, 5);
+        ahead = float3(0, 0, -1);
+        right = float3(1, 0, 0);
+        up = float3(0, 1, 0);
+        fov = 1.5;
+        aspect = 1;
+    }
+    
+    void apply()
+    {
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(fov / 3.14 * 180, aspect, 0.1, 200);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt(eye.x, eye.y, eye.z, eye.x+ahead.x, eye.y+ahead.y, eye.z+ahead.z, 0.0, 1.0, 0.0);
+    }
+};
+
+
+// Object abstract base class.
+class Object
+{
+protected:
+    Material* material;
+    float3 scaleFactor;
+    float3 position;
+    float3 orientationAxis;
+    float orientationAngle;
+public:
+    Object(Material* material):material(material),position(0, 0, 0),orientationAngle(0.0f),scaleFactor(1.0,1.0,1.0),orientationAxis(0.0,1.0,0.0){}
+    virtual ~Object(){}
+    Object* translate(float3 offset){
+        position += offset; return this;
+    }
+    Object* scale(float3 factor){
+        scaleFactor *= factor; return this;
+    }
+    Object* rotate(float angle){
+        orientationAngle += angle; return this;
+    }
+    virtual void draw()
+    {
+        material->apply();
+        // apply scaling, translation and orientation
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glTranslatef(position.x, position.y, position.z);
+        glRotatef(orientationAngle, orientationAxis.x, orientationAxis.y, orientationAxis.z);
+        glScalef(scaleFactor.x, scaleFactor.y, scaleFactor.z);
+        drawModel();
+        glPopMatrix();
+    }
+    virtual void drawModel()=0;
+    virtual void move(double t, double dt){}
+    virtual bool control(std::vector<bool>& keysPressed, std::vector<Object*>& spawn, std::vector<Object*>& objects){return false;}
+};
+
+
+class Teapot : public Object
+{
+public:
+    Teapot(Material* material):Object(material){}
+    void drawModel()
+    {
+        glutSolidTeapot(1.0f);
+    }
+};
+
+class Scene
+{
+    Camera camera;
+    std::vector<LightSource*> lightSources;
+    std::vector<Object*> objects;
+    std::vector<Material*> materials;
+public:
+    Scene()
+    {
+        // BUILD YOUR SCENE HERE
+        lightSources.push_back(
+                               new DirectionalLight(
+                                                    float3(0, 1, 0),
+                                                    float3(1, 0.5, 1)));
+        lightSources.push_back(
+                               new PointLight(
+                                              float3(-1, -1, 1),
+                                              float3(0.2, 0.1, 0.1)));
+        Material* yellowDiffuseMaterial = new Material();
+        materials.push_back(yellowDiffuseMaterial);
+        yellowDiffuseMaterial->kd = float3(1, 1, 0);
+        materials.push_back(new Material());
+        materials.push_back(new Material());
+        materials.push_back(new Material());
+        materials.push_back(new Material());
+        materials.push_back(new Material());
+        materials.push_back(new Material());
+        
+        objects.push_back( (new Teapot( yellowDiffuseMaterial))->translate(float3(2, 0, 0)) );
+        objects.push_back( (new Teapot( materials.at(1) )     )->translate(float3(0, -1, 2))->scale(float3(0.6, 0.6, 0.6)) );
+        objects.push_back( (new Teapot( materials.at(2) )     )->translate(float3(0, 1.2, 0.5))->scale(float3(1.3, 1.3, 1.3)) );
+        
+    }
+    ~Scene()
+    {
+        for (std::vector<LightSource*>::iterator iLightSource = lightSources.begin(); iLightSource != lightSources.end(); ++iLightSource)
+            delete *iLightSource;
+        for (std::vector<Material*>::iterator iMaterial = materials.begin(); iMaterial != materials.end(); ++iMaterial)
+            delete *iMaterial;
+        for (std::vector<Object*>::iterator iObject = objects.begin(); iObject != objects.end(); ++iObject)
+            delete *iObject;
+    }
+    
+public:
+    Camera& getCamera()
+    {
+        return camera;
+    }
+    
+    void draw()
+    {
+        camera.apply();
+        unsigned int iLightSource=0;
+        for (; iLightSource<lightSources.size(); iLightSource++)
+        {
+            glEnable(GL_LIGHT0 + iLightSource);
+            lightSources.at(iLightSource)->apply(GL_LIGHT0 + iLightSource);
+        }
+        for (; iLightSource<GL_MAX_LIGHTS; iLightSource++)
+            glDisable(GL_LIGHT0 + iLightSource);
+        
+        for (unsigned int iObject=0; iObject<objects.size(); iObject++)
+            objects.at(iObject)->draw();
+    }
+};
+
+// global application data
+
+// screen resolution
+const int screenWidth = 600;
+const int screenHeight = 600;
+
+//scene object
+Scene scene;
+
+// Displays the image.
+void onDisplay( ) {
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear screen
+    
+    scene.draw();
+    
+    glutSwapBuffers(); // drawing finished
+}
+
+void onIdle()
+{
+    glutPostRedisplay();
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);						// initialize GLUT
+    glutInitWindowSize(screenWidth, screenHeight);				// startup window size 
+    glutInitWindowPosition(100, 100);           // where to put window on screen
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);    // 8 bit R,G,B,A + double buffer + depth buffer
+    
+    glutCreateWindow("OpenGL teapots");				// application window is created and displayed
+    
+    glViewport(0, 0, screenWidth, screenHeight);
+    
+    glutDisplayFunc(onDisplay);					// register callback
+    glutIdleFunc(onIdle);						// register callback
+    
+    glEnable(GL_LIGHTING);
+    glEnable(GL_DEPTH_TEST);
+    
+    glutMainLoop();								// launch event handling loop
+    
     return 0;
 }
