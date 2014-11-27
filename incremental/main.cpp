5784572056ef011354c135431bb4f3d753e53885
kujenga <ataylor0123@gmail.com>
2014-11-27 11:07:57 +0100

    Initial Commit

---
+++ incremental/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  incremental
+//
+//  Created by Aaron Taylor on 11/27/14.
+//  Copyright (c) 2014 Aaron Taylor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
